97ae414a4dc187f541458b3818b9620a13fa34e0
Kent Riemondy <kent.riemondy@gmail.com>
2017-06-26 16:50:11 -0600

    add fq trim, umi extract, rrna and ercc removal, and star mapping

---
+++ pipeline/snakecharmer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#BSUB -J RNAroids 
+#BSUB -o logs/snakemake_%J.out
+#BSUB -e logs/snakemake_%J.err
+#BSUB -R "select[mem>4] rusage[mem=4] " 
+#BSUB -q normal
+
+set -o nounset -o pipefail -o errexit -x
+
+args=' -q rna -o {log}.out -e {log}.err -J {params.job_name} -R " {params.memory} span[hosts=1] " -n {threads} '
+
+snakemake --drmaa "$args" --snakefile Snakefile --jobs 2 \
+  --latency-wait 50 --rerun-incomplete  --configfile config.yaml 
+  
