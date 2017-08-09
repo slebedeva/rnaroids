83df6143b1795c35b36f33eeb3a0f516102e80d9
Kent Riemondy <kent.riemondy@gmail.com>
2017-08-09 16:08:51 -0600

    add all_threads resource allocation to specify total cores used by entire pipeline. Also use updated umi-tools

--- pipeline/snakecharmer.sh
+++ pipeline/snakecharmer.sh
@@ -9,6 +9,10 @@ set -o nounset -o pipefail -o errexit -x
 
 args=' -q rna -o {log}.out -e {log}.err -J {params.job_name} -R " {params.memory} span[hosts=1] " -n {threads} '
 
-snakemake --drmaa "$args" --snakefile Snakefile --jobs 15 \
-  --latency-wait 50 --rerun-incomplete  --configfile config_rnaseq.yaml 
-  
+snakemake --drmaa "$args" \
+    --snakefile Snakefile \
+    --jobs 50 \
+    --resources all_threads=50 \
+    --latency-wait 50 \
+    --rerun-incomplete  \
+    --configfile config_rnaseq.yaml 
