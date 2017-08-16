4c5370ff9e13b3c8b2776b73367efed3ac0db765
Kent Riemondy <kent.riemondy@gmail.com>
2017-08-16 15:25:06 -0600

    add salmon and kallisto mapping for quick tests

--- pipeline/snakecharmer.sh
+++ pipeline/snakecharmer.sh
@@ -3,16 +3,19 @@
 #BSUB -o logs/snakemake_%J.out
 #BSUB -e logs/snakemake_%J.err
 #BSUB -R "select[mem>4] rusage[mem=4] " 
+#BSUB -m "compute16 "
 #BSUB -q normal
 
 set -o nounset -o pipefail -o errexit -x
 
-args=' -q rna -o {log}.out -e {log}.err -J {params.job_name} -R " {params.memory} span[hosts=1] " -n {threads} '
+args=' -q rna -o {log}.out -e {log}.err -J {params.job_name} -R
+"{params.memory} span[hosts=1] " -n {threads} -m "compute10 compute11 compute12 compute13 compute14 compute15 compute16 compute09 compute08 compute06 compute05 compute07" '
+    
 
 snakemake --drmaa "$args" \
     --snakefile Snakefile \
-    --jobs 50 \
-    --resources all_threads=50 \
+    --jobs 72 \
+    --resources all_threads=84 \
     --latency-wait 50 \
     --rerun-incomplete  \
     --configfile config_rnaseq.yaml 
