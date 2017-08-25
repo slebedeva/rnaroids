0628244438ff15152c96c967b3b12eddaca99701
Kent Riemondy <kent.riemondy@gmail.com>
2017-08-25 09:04:26 -0600

    add in 45s rRNA sequence

--- dbases/hg19/rRNA/getrRNA.sh
+++ dbases/hg19/rRNA/getrRNA.sh
@@ -4,8 +4,10 @@
 #BSUB -q normal
 
 # extract out rRNA sequences (defined by gene_biotype:rRNA)
+# add in 45srRNA repeat sequence also
 
 gunzip -c Homo_sapiens.GRCh38.ncrna.fa.gz \
     | fasta_formatter -t \
     | awk '{FS = "\t"} $1 ~ /gene_biotype:rRNA/ {print ">"$1"\n"$2}' \
-    | > Homo_sapiens.GRCh38.rRNA.fa
+    | cat - 45srRNA.fa > Homo_sapiens.GRCh38.rRNA.fa
+
